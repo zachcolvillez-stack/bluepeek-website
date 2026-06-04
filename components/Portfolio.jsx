1459ecb3bc10197772f70ff7f76258be7a34e741
Zach Colville <zach.colvillez@gmail.com>
2026-06-04 15:33:56 +0800

    auto-save: Portfolio.jsx

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -54,7 +54,7 @@ export default function Portfolio() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-5">
           {PROJECTS.map((p) => (
             <a key={p.title}
               href={p.url}
